20b8cb55e7a9fd09b1a8d5918e5fa907dd855a0e
shanepearson62 <108286163+shanepearson62@users.noreply.github.com>
2023-08-02 15:14:23 -0400

    Editing Bullet Lifetime
    Changed Bullet lifetime in the BulletScript from 2.0f to 0.9f. Done so to limit the time the bullet survives off screen.

--- Assets/Scripts/Player and Game Control/BulletScript.cs
+++ Assets/Scripts/Player and Game Control/BulletScript.cs
@@ -6,31 +6,36 @@ using Photon.Pun;
 public class BulletScript : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
-    public float maxLifetime = 2.0f;
+    public float maxLifetime = 0.9f;
     public float speed = 500.0f;
     PhotonView view;
     public PlayerScript player;
 
-    private void Start() {
-         view = GetComponent<PhotonView>();
+    private void Start()
+    {
+        view = GetComponent<PhotonView>();
     }
 
-    public void addPlayer(PlayerScript p){
+    public void addPlayer(PlayerScript p)
+    {
         player = p;
     }
 
-    private void Awake() {
-       
+    private void Awake()
+    {
+
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    public void Project(Vector2 direction) {
+    public void Project(Vector2 direction)
+    {
         _rigidbody.AddForce(direction * this.speed);
-        
+
         Destroy(this.gameObject, this.maxLifetime);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         Destroy(this.gameObject);
     }
 }
